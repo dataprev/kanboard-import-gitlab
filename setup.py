1ae72ffa1634c45cc31c29b6914b8251af38c249
Juracy Filho <juracy.filho@dataprev.gov.br>
2017-04-20 09:43:59 -0300

    Setup for pypi

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='kanboard-gitlab',
+    packages=['kanboard_gitlab'],
+    version='0.2',
+    description='Script for migrating gitlab issues to kanboard',
+    author='Juracy Filho',
+    author_email='juracy@gmail.com',
+    url='https://github.com/dataprev/kanboard-import-gitlab',
+    download_url='https://github.com/dataprev/kanboard-import-gitlab/archive/0.2.tar.gz',
+    keywords=['gitlab', 'migration', 'kanboard'],
+    classifiers=[],
+    scripts=['kanboard_gitlab/kanboard-gitlab'],
+)
